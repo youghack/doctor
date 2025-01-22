c3db03af548cc34b658018f99d7402747331cb10
youghack <hacker.youg0@example.com>
2025-01-22 19:50:52 +0545

    icon issue resolved

--- resources/views/user/location.blade.php
+++ resources/views/user/location.blade.php
@@ -3,7 +3,8 @@
         <div class="row">
             <h1 class="text-center wow fadeInUp">Our Location</h1>
             <div class="col-md-12 my-3 map zoomIn">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3563.206535806678!2d85.91815501484874!3d26.73778448320321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ec6b4e2534f605%3A0x751dd33ec8017124!2sSj%20Computer%20Center!5e0!3m2!1sen!2snp!4v1651289644030!5m2!1sen!2snp" width="100%" height="450" style="border:0;" allowfullscreen="true" loading="lazy"></iframe>
+                {{-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3563.206535806678!2d85.91815501484874!3d26.73778448320321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ec6b4e2534f605%3A0x751dd33ec8017124!2sSj%20Computer%20Center!5e0!3m2!1sen!2snp!4v1651289644030!5m2!1sen!2snp" width="100%" height="450" style="border:0;" allowfullscreen="true" loading="lazy"></iframe> --}}
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3563.167492561309!2d85.91410627443445!3d26.739030667534813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ec6b284cc2505d%3A0xccc70974881bb727!2sRajarshi%20Janak%20University!5e0!3m2!1sen!2snp!4v1737550826261!5m2!1sen!2snp" width="100%" height="450" style="border:0;" allowfullscreen="true" loading="lazy"></iframe>
         </div>
     </div>
 </section>
